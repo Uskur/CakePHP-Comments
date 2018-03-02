1936c195985fdb8a97c3063b887c92d8636bf344
burak <burak@xps13>
2018-03-02 18:30:32 +0300

    class not passed to element

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -38,14 +38,14 @@ class CommentsController extends AppController
 
             // check if we can comment this content
             if (!$model->hasBehavior('Commentable') || $model->hasBehavior('Commentable') && !$model->exists(['id' => $data['ref_id']])) {
-                $this->Flash->set(__("You can't comment this"), ['class' => 'error', 'element' => 'Kareylo/Comments.comment']);
+                $this->Flash->set(__("You can't comment this"), ['element' => 'Kareylo/Comments.comment_error']);
 
                 return $this->redirect($referer);
             }
 
             // Check if parent exists with the correct model
             if ($data['parent_id'] && !$this->Comments->exists(['id' => $data['parent_id'], 'ref' => $data['ref']])) {
-                $this->Flash->set(__("You can't answer to this comment !"), ['class' => 'error', 'element' => 'Kareylo/Comments.comment']);
+                $this->Flash->set(__("You can't answer to this comment !"), ['element' => 'Kareylo/Comments.comment_error']);
 
                 return $this->redirect($referer);
             }
@@ -53,9 +53,9 @@ class CommentsController extends AppController
             $comment = $model->Comments->newEntity();
             $comment = $model->Comments->patchEntity($comment, $data);
             if ($model->Comments->save($comment)) {
-                $this->Flash->set(__('Your comment has been correctly added !'), ['class' => 'success', 'element' => 'Kareylo/Comments.comment']);
+                $this->Flash->set(__('Your comment has been correctly added !'), ['element' => 'Kareylo/Comments.comment_success']);
             } else {
-                $this->Flash->set(__('An error occured while saving your comment ! '), ['class' => 'error', 'element' => 'Kareylo/Comments.comment']);
+                $this->Flash->set(__('An error occured while saving your comment ! '), ['element' => 'Kareylo/Comments.comment_error']);
             }
         }
 
